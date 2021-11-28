8515efe0aaf2450132f228d881a855d4250fad7a
Jim Lougheed <jims.codes@gmail.com>
2021-11-28 18:28:47 -0500

    Apply basic styling to ListedRecipe component for visualization

--- client/src/components/ListedRecipe/ListedRecipe.jsx
+++ client/src/components/ListedRecipe/ListedRecipe.jsx
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom';
 
+import './ListedRecipe.scss';
+
 function ListedRecipe({ componentClassName, recipe: { uri, label, image, ingredientLines} }) {
     return (
-        <li className={`${componentClassName}__item`}>
-            <Link to={`/recipe/${(uri.substr(51,50))}`}>
-                <h1 className={`${componentClassName}__item-name`}>{label}</h1>
-            </Link>
+        <li>
+            <Link className={`${componentClassName}__item`} to={`/recipe/${(uri.substr(51,50))}`}>
             <img className={`${componentClassName}__item-image`} src={image} alt={label} />
-            {ingredientLines.map((ingredientLine) => {
-                return <p className={`${componentClassName}__item-ingredients`}>{ingredientLine}</p>
-            })}
+            <h1 className={`${componentClassName}__item-name`}>{label}</h1>
+            <div className={`${componentClassName}__item-ingredients-container`}>
+                {ingredientLines.map((ingredientLine) => {
+                    return <p className={`${componentClassName}__item-ingredients`}>{ingredientLine}</p>
+                })}
+            </div>
+            </Link>
         </li>
     )
 }
